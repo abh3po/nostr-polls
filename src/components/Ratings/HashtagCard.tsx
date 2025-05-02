366e5c33ff8a0f5cff8df74d2d57703b73047163
abhay <toabhayraizada@gmail.com>
2025-05-02 15:11:40 +0530

    Open hashtag feed on pollerama

--- src/components/Ratings/HashtagCard.tsx
+++ src/components/Ratings/HashtagCard.tsx
@@ -2,23 +2,18 @@ import React from "react";
 import { Card, CardContent, Typography } from "@mui/material";
 import Rate from "./Rate";
 
-// const fakeEvent = (tag: string) => ({
-//   kind: 34259,
-//   id: tag,
-//   pubkey: "",
-//   created_at: 0,
-//   content: "",
-//   sig: "",
-//   tags: [
-//     ["d", tag],
-//     ["m", "hashtag"],
-//   ],
-// });
-
 const HashtagCard: React.FC<{ tag: string }> = ({ tag }) => (
   <Card sx={{ mb: 2 }}>
     <CardContent>
-      <Typography variant="h6">{tag}</Typography>
+      <Typography
+        variant="h6"
+        style={{ textUnderlineOffset: "0.1em", cursor: "pointer" }}
+        onClick={() =>
+          window.open(`https://zapddit.com/t;topic=${tag}`, "_blank")
+        }
+      >
+        {tag}
+      </Typography>
       <Rate entityId={tag} entityType="hashtag" />
     </CardContent>
   </Card>
