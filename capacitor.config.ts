2a21f03acfc25eb07582768e89536bde7b507826
abhay <toabhayraizada@gmail.com>
2026-01-10 10:06:47 +0530

    Working ionic capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.formstr.pollerama',
+  appName: 'Pollerama',
+  webDir: 'build'
+};
+
+export default config;
